380d65546b9ad46b647140f61eef07e8508709e6
MaksGovor <maksgovruha@gmail.com>
2023-01-14 22:39:07 +0200

    Fix: fixed styled

--- adc-fe/src/components/NavHeader/NavHeader.tsx
+++ adc-fe/src/components/NavHeader/NavHeader.tsx
@@ -1,12 +1,12 @@
+import Container from '@mui/material/Container';
 import React from 'react';
 
 import { FlexBox, NavToolbar } from './NavHeader.style';
 
-console.log(FlexBox);
 export default function NavHeader() {
   return (
     <FlexBox>
-      <div>a</div>
+      <Container></Container>
     </FlexBox>
   );
 }

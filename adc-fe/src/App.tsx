ec864a60d20e8b2bc3e68649393d2747646ab395
MaksGovor <maksgovruha@gmail.com>
2023-02-02 21:14:29 +0200

    Feat: FE auth with auth0

--- adc-fe/src/App.tsx
+++ adc-fe/src/App.tsx
@@ -1,3 +1,4 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import {
   StyledEngineProvider,
   ThemeProvider as MuiThemeProvider,
@@ -12,7 +13,17 @@ function App() {
     <StyledThemeProvider theme={theme}>
       <StyledEngineProvider injectFirst>
         <MuiThemeProvider theme={theme}>
-          <Login />
+          <Auth0Provider
+            domain={import.meta.env.VITE_AUTH0_DOMAIN}
+            clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+            authorizationParams={{
+              redirect_uri: window.location.origin,
+              audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+              scope: 'offline_access',
+            }}
+          >
+            <Login />
+          </Auth0Provider>
         </MuiThemeProvider>
       </StyledEngineProvider>
     </StyledThemeProvider>
--- adc-fe/src/pages/Login/Login.tsx
+++ adc-fe/src/pages/Login/Login.tsx
@@ -1,11 +1,27 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
 
 import NavHeader from '../../components/NavHeader';
 
 export default function Login() {
+  const { loginWithRedirect, logout, isAuthenticated, getAccessTokenSilently } =
+    useAuth0();
+
+  if (isAuthenticated) {
+    (async () => {
+      console.log(await getAccessTokenSilently());
+    })();
+  }
+
   return (
     <>
       <NavHeader></NavHeader>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+      <button
+        onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
+      >
+        Log Out
+      </button>
     </>
   );
 }

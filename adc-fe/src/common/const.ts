540e4e18b3d0b35655f4b469897861ee04d83d71
MaksGovor <maksgovruha@gmail.com>
2023-05-29 02:14:38 +0300

    FE Fix: fix max len preview card text

--- adc-fe/src/common/const.ts
+++ adc-fe/src/common/const.ts
@@ -2,6 +2,6 @@ export const POST_LIMIT_PER_PAGE = 40;
 
 export const COMMENT_LIMIT_PER_PAGE = 30;
 
-export const TEXT_PREVIEW_MAX_LEN = 300;
+export const TEXT_PREVIEW_MAX_LEN = 250;
 
 export const MEDIA_PATH = import.meta.env.VITE_APP_MEDIA_PATH;
